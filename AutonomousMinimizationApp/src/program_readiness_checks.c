4aee75dbf66ff9403e02811edb698c4200e2c664
chris garnier <cgarnier@slac.stanford.edu>
2025-01-10 15:18:46 -0800

    tpro

--- AutonomousMinimizationApp/src/program_readiness_checks.c
+++ AutonomousMinimizationApp/src/program_readiness_checks.c
@@ -14,8 +14,8 @@ static long subChecksProcess(struct subRecord *psub){
     // Access inputs
     int inpa = psub->a;
     // Print the values to the IOC console
-    printf("subRecord '%s' called with INPA=%.2f\n", psub->name, inpa);
-
+    //printf("subRecord '%s' called with INPA=%.2f\n", psub->name, inpa);
+    print('called subChecksProcess')
     // Perform optional computation (here, we'll set the VAL field to INPA + INPB)
 
     psub->val = inpa;
