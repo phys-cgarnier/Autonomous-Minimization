e1668c43e7cd04c9d6aa1ec038e4474ac6a5f5e9
Chris Garnier <cgarnier@slac.stanford.edu>
2025-01-29 13:07:27 -0800

    can load in serialized model however epics still does not like it because of the use of field_validators

---
+++ python3.10/rhel7-x86_64/magnet_generators.py
@@ -0,0 +1,8 @@
+from generators.device_generator import Generator
+from typing import List, Dict, Any
+
+class MagnetGenerator(Generator):
+    magnet_types: List = ['XCOR', 'YCOR', 'QUAD', 'KICK']
+
+    def config_contents(self):
+        return None
