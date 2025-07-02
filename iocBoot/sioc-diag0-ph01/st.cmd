6bd6ea0a4dfe1ef64bd3e533712717a5bab959b7
Chris Garnier <cgarnier@slac.stanford.edu>
2025-07-02 14:04:45 -0700

    added new ioc with name approved by naming team?

--- iocBoot/sioc-diag0-ph01/st.cmd
+++ iocBoot/sioc-diag0-ph01/st.cmd
@@ -6,15 +6,14 @@
 < envPaths
 
 cd "${TOP}"
-#epicsEnvSet(PYTHONPATH, "$(PYTHONPATH):$(EPICS_SITE_TOP)/R7.0.3.1-1.0/modules/pyDevSup/R1.2-0.0.2PY3.10/python3.10/rhel7-x86_64:../../../../lcls-tools")
-#epicsEnvSet(PYTHONPATH, "$(PYTHONPATH):/sdf/home/c/cgarnier/autonomous_diag0/lcls-tools")
-#epicsEnvSet(BEAMLINE, "DIAG0")
-
+epicsEnvSet(PYTHONPATH, "$(PYTHONPATH):$(EPICS_SITE_TOP)/R7.0.3.1-1.0/modules/pyDevSup/R1.2-0.0.2PY3.10/python3.10/rhel7-x86_64")
 
 epicsEnvSet("EPICS_CA_AUTO_ADDR_LIST","NO")
+
 epicsEnvSet("EPICS_CA_ADDR_LIST", "lcls-prod01:5068 lcls-prod01:5063 mcc-dmz")
-epicsEnvShow("LD_LIBRARY_PATH")
 #epicsEnvSet("EPICS_CA_REPEATER_PORT","5069")
+# for setup on dev-srv09 comment out line about and uncomment these
+
 #epicsEnvSet("EPICS_CA_ADDR_LIST", "127.0.0.1")
 #epicsEnvSet("EPICS_CA_SERVER_PORT", "10514")
 
